e590c73d2f6dbbd2a1da56d7ed24a23c58512f55
bianca <bianca.subion@gmail.com>
2016-09-10 05:44:22 -0700

    added some stuff like delete and view

--- server.py
+++ server.py
@@ -47,4 +47,4 @@ def delete(entry):
     return redirect('/', code = 302)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='127.0.0.1', port = 80)
