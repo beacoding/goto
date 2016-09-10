eb8aebe0b7028c88aabad6ca0e309f7377bc11fb
bianca <bianca.subion@gmail.com>
2016-09-10 05:11:49 -0700

    yay

--- server.py
+++ server.py
@@ -7,11 +7,18 @@ conn = sqlite3.connect('example.db')
 c = conn.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS short_routes (id INTEGER PRIMARY KEY AUTOINCREMENT, alias text, url text)")
 conn.commit()
-#users_map = {"/bea" => "https://www.messenger.com/t/bea.subion"}
 
 @app.route('/')
-def index(): 
-    return render_template('mini_routes.html')
+def index():
+
+    con = sqlite3.connect("example.db")
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("select * from short_routes")
+
+    rows = cur.fetchall();
+    return render_template('mini_routes.html', rows = rows)
 
 @app.route('/add_new_route', methods=['GET', 'POST'])
 def add_new_route():
@@ -23,9 +30,6 @@ def add_new_route():
     conn.commit()
     return redirect('/', code = 302)
 
-@app.route('/bea')
-def bea():
-    return redirect("https://www.messenger.com/t/bea.subion", code = 302)
 
 @app.route('/<alias>')
 def redir(alias):
@@ -34,5 +38,13 @@ def redir(alias):
     redirect_url = c.execute("SELECT url FROM short_routes WHERE alias=?", (alias,)).fetchone()
     return redirect('%s' % redirect_url, code = 302)
 
+@app.route('/delete/<entry>')
+def delete(entry):
+    con = sqlite3.connect("example.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM short_routes WHERE alias = ?", (entry,))
+    con.commit()
+    return redirect('/', code = 302)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=80)
+    app.run(debug=True, host='127.0.0.1')
