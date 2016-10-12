26ff75e1000b81af222783a1d38fd41ffd7a9050
bianca <bianca.subion@gmail.com>
2016-10-12 12:14:33 -0700

    edited setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name         = 'goto',
-    version      = '1.0.5',
+    version      = '1.0.8',
     author       = 'Bianca Subion',
     author_email = 'biancasubion@gmail.com',
     license      = 'GPL3',
